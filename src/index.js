b90268fe89fe6c6866b4fb70dc7c7c9e0d6dd1ee
krissen95 <krissen@hotmail.no>
2022-08-28 11:23:31 +0200

    Basic Svg spawning

--- src/index.js
+++ src/index.js
@@ -1,36 +1,67 @@
-    /*UI functionality*/
 let toggleFile = false;
 let toggleEdit = false;
-let toggSvgCircle = false;
+var toggleSvgCircle = false;
+const screenClick = document.querySelector('.Content');
+
+/*Canvas Svg Element definition*/ 
+const node = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+node.setAttribute("class", "svgContent");
+node.setAttribute("width", "100");
+node.setAttribute("height", "100");
+//const circle = document.createElement("circle");
+const circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
+circle.setAttribute("cx" , "50");
+circle.setAttribute("cy" , "50");
+circle.setAttribute("r" , "40");
+circle.setAttribute("stroke" , "green");
+circle.setAttribute("stroke-width","4");
+circle.setAttribute("fill","yellow");
+node.appendChild(circle);
+
+/*Main Menu*/
     function btnToggle(toggleContent){
         let togglebox = document.querySelector(toggleContent);
         if(toggleFile){
-            toggleFile=false;
+            toggleFile= !toggleFile;
             togglebox.setAttribute('style','display:none');
         }
         else if(!toggleFile){
-            toggleFile=true;
+            toggleFile= !toggleFile
             togglebox.setAttribute('style','display:block');
         }
     }
 
-    function btnSvgCircle(toggleContent){
-        let togglebox = document.querySelector(toggleContent);
-        if(toggleFile){
-            toggleFile=false;
-            togglebox.setAttribute('style','outline:none');
+    /*SVG Tool button circle*/
+    function btnSvgCircle(event){
+        let toggle = document.querySelector('.btnSvgCircleToggle');
+        event.stopPropagation();
+        if(toggleSvgCircle){
+            console.log("circle set to false");
+            toggleSvgCircle= !toggleSvgCircle;
+            toggle.setAttribute('style','outline:none');
+            screenClick.removeEventListener("click", AddSVGCircle);
         }
-        else if(!toggleFile){
-            toggleFile=true;
-            togglebox.setAttribute('style','background-color: white;');
-            let screenClick = document.querySelector(toggleContent);
-            screenClick.addEventListener('onclick', spawnCircle);
-            console.log("click");
-            
+        else if(!toggleSvgCircle){
+            console.log("circle set to true");
+            toggleSvgCircle= !toggleSvgCircle;
+            toggle.setAttribute('style','background-color: white;');
         }
     }
-
-
-    function spawnCircle(e){
+function AddSVGCircle(event){
+    if(!event.detail||event.detail==1){
         console.log("spawn");
-    };
+        console.log(event.clientY, event.clientX);
+        screenClick.appendChild(node);
+    }
+}
+
+   /*Master Tool Manager*/
+    function WorkMode(){
+        if(toggleSvgCircle === true){screenClick.addEventListener("click", AddSVGCircle);}
+
+        window.requestAnimationFrame(WorkMode);
+    }
+
+    /*Canvas Update Loop*/
+    WorkMode();
+    
